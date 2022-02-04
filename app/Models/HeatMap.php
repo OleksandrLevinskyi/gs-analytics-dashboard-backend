2fce1bf469638ba35be402c756db94eeca1c8c22
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-02-04 11:46:01 -0500

    finilize heatmap endpoint

--- app/Models/HeatMap.php
+++ app/Models/HeatMap.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Array_;
+use function PHPUnit\Framework\isEmpty;
 
 class HeatMap extends Model
 {
@@ -12,9 +14,15 @@ class HeatMap extends Model
 
     static function getData()
     {
-        return DB::select(
+        $users = DB::table('users')
+            ->select('id')
+            ->where('is_vehikl_member', 1)
+            ->get()
+            ->pluck('id');
+
+        $connections = DB::select(
             DB::raw(
-                "SELECT user_name1 source, user_name2 target, COUNT(data.user_id1) weight
+                "SELECT user_id1 source, user_id2 target, COUNT(data.user_id1) weight
                         FROM (
                           SELECT gsu1.user_id user_id1,
                                  gsu1.name user_name1,
@@ -41,5 +49,27 @@ class HeatMap extends Model
                         GROUP BY data.user_id1, data.user_id2;"
             )
         );
+
+        $result = [];
+        foreach ($users as $row) {
+            foreach ($users as $col) {
+                if ($row > $col) {
+                    $result[] = ['source' => $row, 'target' => $col, 'weight' => 0];
+                }
+            }
+        }
+
+
+        foreach ($result as &$elem) {
+            foreach ($connections as $connection) {
+                if ($connection->source > $connection->target ?
+                    $elem['source'] === $connection->source && $elem['target'] === $connection->target :
+                    $elem['source'] === $connection->target && $elem['target'] === $connection->source) {
+                    $elem['weight'] = $connection->weight;
+                }
+            }
+        }
+
+        return $result;
     }
 }
