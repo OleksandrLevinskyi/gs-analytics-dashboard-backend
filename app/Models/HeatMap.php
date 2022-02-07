041da1181742d48831255ab4f5fd49abe5e0b458
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-02-07 15:53:13 -0500

    resolve duplication bug with heatmap

--- app/Models/HeatMap.php
+++ app/Models/HeatMap.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use phpDocumentor\Reflection\Types\Array_;
-use function PHPUnit\Framework\isEmpty;
 
 class HeatMap extends Model
 {
@@ -14,15 +12,21 @@ class HeatMap extends Model
 
     static function getData()
     {
+        $userBlackList = [18, 30, 42, 55, 60, 83, 106];
+
         $users = DB::table('users')
-            ->select('id')
+            ->selectRaw('MIN(id) id, name')
             ->where('is_vehikl_member', 1)
-            ->get()
-            ->pluck('id');
+            ->whereNotIn('id', $userBlackList)
+            ->orderBy('id')
+            ->groupBy('name')
+            ->get();
+
+
 
         $connections = DB::select(
             DB::raw(
-                "SELECT user_id1 source, user_id2 target, COUNT(data.user_id1) weight
+                "SELECT user_id1 source_id, user_id2 target_id, user_name1 source, user_name2 target, COUNT(data.user_id1) weight
                         FROM (
                           SELECT gsu1.user_id user_id1,
                                  gsu1.name user_name1,
@@ -43,28 +47,42 @@ class HeatMap extends Model
                           ON gsu1.growth_session_id = gsu2.growth_session_id AND gsu1.user_id <> gsu2.user_id
                             JOIN growth_sessions gs ON gs.id = gsu1.growth_session_id
                         ) data
-                        JOIN users u1 ON u1.id = data.user_id1
-                        JOIN users u2 ON u2.id = data.user_id2
                         WHERE user_id1 > user_id2
                         GROUP BY data.user_id1, data.user_id2;"
             )
         );
 
         $result = [];
-        foreach ($users as $row) {
+//        for ($i = $users->count() - 1; $i >= 0; $i--) {
+//            $row = $users[$i];
+//            foreach ($users as $col) {
+//                if ($row > $col) {
+//                    $result[] = ['source' => $row, 'target' => $col, 'weight' => 0];
+//                }
+//            }
+//        }
+
+
+        for ($i = $users->count() - 1; $i >= 0; $i--) {
+            $row = $users[$i];
             foreach ($users as $col) {
-                if ($row > $col) {
-                    $result[] = ['source' => $row, 'target' => $col, 'weight' => 0];
+                if ($row >= $col) {
+                    $result[] = [
+                        'source_id' => $row->id,
+                        'source' => $row->name,
+                        'target_id' => $col->id,
+                        'target' => $col->name,
+                        'weight' => 0
+                    ];
                 }
             }
         }
 
-
         foreach ($result as &$elem) {
             foreach ($connections as $connection) {
-                if ($connection->source > $connection->target ?
-                    $elem['source'] === $connection->source && $elem['target'] === $connection->target :
-                    $elem['source'] === $connection->target && $elem['target'] === $connection->source) {
+                if ($connection->source_id > $connection->target_id ?
+                    $elem['source_id'] === $connection->source_id && $elem['target_id'] === $connection->target_id :
+                    $elem['source_id'] === $connection->target_id && $elem['target_id'] === $connection->source_id) {
                     $elem['weight'] = $connection->weight;
                 }
             }
