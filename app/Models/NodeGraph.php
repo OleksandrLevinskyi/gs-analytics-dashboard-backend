fbcc7ca4ea07a9e05d9e4e01be5103f948ad31f4
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-03-21 15:42:15 -0400

    optimize NodeGraph getData

--- app/Models/NodeGraph.php
+++ app/Models/NodeGraph.php
@@ -19,36 +19,13 @@ class NodeGraph extends Model
 
         $edges = DB::select(
             DB::raw(
-//                "SELECT user_id1 source, user_id2 target, COUNT(data.user_id1) weight
-//                FROM (
-//                    SELECT gsu1.user_id user_id1, gsu2.user_id user_id2
-//                    FROM growth_session_user gsu1
-//                    JOIN growth_session_user gsu2 ON gsu1.growth_session_id = gsu2.growth_session_id AND gsu1.user_id <> gsu2.user_id
-//                    ) data
-//                WHERE user_id1 > user_id2 AND user_id1 <=50 AND user_id2<=50
-//                GROUP BY data.user_id1, data.user_id2;"
-                "SELECT user_id1 source, user_id2 target, COUNT(data.user_id1) weight
-                        FROM (
-                          SELECT gsu1.user_id user_id1, gsu2.user_id user_id2
-                          FROM (
-                            SELECT *
-                            FROM growth_session_user gsu
-                            JOIN users u ON u.id = gsu.user_id
-                            WHERE u.is_vehikl_member = 1
-                          ) gsu1
-                          JOIN (
-                            SELECT *
-                            FROM growth_session_user gsu
-                            JOIN users u ON u.id = gsu.user_id
-                            WHERE u.is_vehikl_member = 1
-                          ) gsu2
-                          ON gsu1.growth_session_id = gsu2.growth_session_id AND gsu1.user_id <> gsu2.user_id
-                            JOIN growth_sessions gs ON gs.id = gsu1.growth_session_id
-                        ) data
-                        JOIN users u1 ON u1.id = data.user_id1
-                        JOIN users u2 ON u2.id = data.user_id2
-                        WHERE user_id1 > user_id2
-                        GROUP BY data.user_id1, data.user_id2;"
+                "SELECT gsu1.user_id source, gsu2.user_id target, COUNT(CONCAT(gsu1.user_id, '_', gsu2.user_id)) weight
+                        FROM view_name gsu1
+                        JOIN view_name gsu2
+                        ON gsu1.growth_session_id = gsu2.growth_session_id AND gsu1.user_id <> gsu2.user_id
+                        JOIN growth_sessions gs ON gs.id = gsu1.growth_session_id
+                        WHERE gsu1.user_id > gsu2.user_id
+                        GROUP BY source, target;"
             )
         );
 
