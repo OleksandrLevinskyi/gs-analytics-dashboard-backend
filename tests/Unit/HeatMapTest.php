170ae92f9ca43b52fdd6c8b9e0a003eceb2c1b91
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-03-24 16:26:56 -0400

    add heat map test

---
+++ tests/Unit/HeatMapTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\HeatMap;
+use App\Models\NodeGraph;
+use Tests\TestCase;
+
+class HeatMapTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_it_returns_correct_data()
+    {
+        $data = HeatMap::getData();
+
+        $expected = json_decode(file_get_contents(base_path('tests/Unit/heatmap.json')), true);
+
+        $this->assertCount(2556, $data);
+        collect($data)->every(function ($item, $idx) use ($expected) {
+            $item = (array)$item;
+            $this->assertEquals($item, $expected[$idx]);
+        });
+    }
+}
